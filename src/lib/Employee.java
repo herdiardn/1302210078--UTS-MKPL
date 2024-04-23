e42bbba5114ed3e7ce95702216c9aece485f79a0
herdiardn <121550473+herdiardn@users.noreply.github.com>
2024-04-24 01:10:10 +0700

    Primitive obsession update 2
    Menggunakan objek LocalDate untuk merepresentasikan tanggal.

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -13,13 +13,13 @@ public class Employee {
 	private String idNumber;
 	private String address;
 	
-	private int yearJoined;
-	private int monthJoined;
-	private int dayJoined;
+	private LocalDate joinedDate;
 	private int monthWorkingInYear;
 	
 	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
+	private enum Gender{
+		male, female
+	}
 	
 	private int monthlySalary;
 	private int otherMonthlyIncome;
@@ -30,16 +30,17 @@ public class Employee {
 
 	private List<String> childNames;
 	private List<String> childIdNumbers;
+	private Gender gender;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, Gender gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.idNumber = idNumber;
 		this.address = address;
-		this.yearJoined = yearJoined;
-		this.monthJoined = monthJoined;
-		this.dayJoined = dayJoined;
+		//this.yearJoined = yearJoined;
+		//this.monthJoined = monthJoined;
+		//this.dayJoined = dayJoined;
 		this.isForeigner = isForeigner;
 		this.gender = gender;
 		
@@ -94,11 +95,11 @@ public class Employee {
 		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
 		LocalDate date = LocalDate.now();
 		
-		if (date.getYear() == yearJoined) {
-			monthWorkingInYear = date.getMonthValue() - monthJoined;
-		}else {
-			monthWorkingInYear = 12;
-		}
+		if (date.getYear() == joinedDate.getYear()) {
+            monthWorkingInYear = date.getMonthValue() - joinedDate.getMonthValue();
+        } else {
+            monthWorkingInYear = 12;
+        }
 		
 		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
 	}
